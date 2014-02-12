2fe76957798277c98174761e5daf69f2a441714e
YourdonJou <yourdonjou@gmail.com>
2014-02-12 15:49:53 -0800

    question pack class update 1

--- Friend Quiz/QuestionPack.h
+++ Friend Quiz/QuestionPack.h
@@ -10,4 +10,50 @@
 
 @interface QuestionPack : NSObject
 
+@property int questionPackID;
+
+// Question 1
+@property NSString *question1Question;
+
+@property NSArray *question1SuggestedAnswers;
+@property NSArray *question1Answers;
+
+@property int question1CorrectAnswer;
+
+
+// Question 2
+@property NSString *question2Question;
+
+@property NSArray *question2SuggestedAnswers;
+@property NSArray *question2Answers;
+
+@property int question2CorrectAnswer;
+
+
+// Question 3
+@property NSString *question3Question;
+
+@property NSArray *question3SuggestedAnswers;
+@property NSArray *question3Answers;
+
+@property int question3CorrectAnswer;
+
+
+// Question 4
+@property NSString *question4Question;
+
+@property NSArray *question4SuggestedAnswers;
+@property NSArray *question4Answers;
+
+@property int question4CorrectAnswer;
+
+
+// Question 5
+@property NSString *question5Question;
+
+@property NSArray *question5SuggestedAnswers;
+@property NSArray *question5Answers;
+
+@property int question5CorrectAnswer;
+
 @end

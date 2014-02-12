3f99d50eedc4f0e3bdc9ae72fb4cdac244689dab
YourdonJou <yourdonjou@gmail.com>
2014-02-12 15:57:56 -0800

    2

--- Friend Quiz/QuestionPack.m
+++ Friend Quiz/QuestionPack.m
@@ -10,4 +10,20 @@
 
 @implementation QuestionPack
 
+@synthesize questionPackID,
+question1Question, question1Answers, question1CorrectAnswer, question1SuggestedAnswers,
+question2Question, question2Answers, question2CorrectAnswer, question2SuggestedAnswers,
+question3Question, question3Answers, question3CorrectAnswer, question3SuggestedAnswers,
+question4Question, question4Answers, question4CorrectAnswer, question4SuggestedAnswers,
+question5Question, question5Answers, question5CorrectAnswer, question5SuggestedAnswers;
+
+-(void)enterQuestionPackIDandGetInfoFromDatabase: (int)packID
+{
+    questionPackID = packID;
+    
+    //Database code
+    
+
+}
+
 @end

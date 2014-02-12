aca768c2cccd9d2d43268a9fcec4c5a5264f676d
YourdonJou <yourdonjou@gmail.com>
2014-02-12 15:23:31 -0800

    add question pack object

---
+++ Friend Quiz/QuestionPack.h
@@ -0,0 +1,13 @@
+//
+//  QuestionPack.h
+//  Friend Quiz
+//
+//  Created by Shi Yo Jou on 2014/2/12.
+//  Copyright (c) 2014年 Christian Retureta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuestionPack : NSObject
+
+@end
---
+++ Friend Quiz/QuestionPack.m
@@ -0,0 +1,13 @@
+//
+//  QuestionPack.m
+//  Friend Quiz
+//
+//  Created by Shi Yo Jou on 2014/2/12.
+//  Copyright (c) 2014年 Christian Retureta. All rights reserved.
+//
+
+#import "QuestionPack.h"
+
+@implementation QuestionPack
+
+@end

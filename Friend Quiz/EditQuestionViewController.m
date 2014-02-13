15ce495c73e2be21fc483387b288eac3c2af313e
YourdonJou <yourdonjou@gmail.com>
2014-02-12 16:12:06 -0800

    4

---
+++ Friend Quiz/EditQuestionViewController.m
@@ -0,0 +1,38 @@
+//
+//  EditQuestionViewController.m
+//  Friend Quiz
+//
+//  Created by Shi Yo Jou on 2014/2/12.
+//  Copyright (c) 2014年 Christian Retureta. All rights reserved.
+//
+
+#import "EditQuestionViewController.h"
+
+@interface EditQuestionViewController ()
+
+@end
+
+@implementation EditQuestionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

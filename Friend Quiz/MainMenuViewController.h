29be278d938f0ba11aa57d951162def027cd4c9d
bobbyloo92 <christian.retureta@gmail.com>
2014-02-13 20:08:43 -0800

    Fixed the Storyboard
    Storyboard now shows up again. Had to revert back previous change.

--- Friend Quiz/MainMenuViewController.h
+++ Friend Quiz/MainMenuViewController.h
@@ -5,6 +5,7 @@
 //  Created by Christian Retureta on 2014-02-10.
 //  Copyright (c) 2014 Christian Retureta. All rights reserved.
 //
+//
 
 #import <UIKit/UIKit.h>
 #import <FacebookSDK/FacebookSDK.h>

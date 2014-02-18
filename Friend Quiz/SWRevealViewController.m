02985f95cb9fbd4505d362307f2836b6b1f23771
bobbyloo92 <christian.retureta@gmail.com>
2014-02-17 20:25:59 -0800

    Fixed the FB Picture
    The picture now shows up after added the log in view and placing it as
    a delegate

--- Friend Quiz/SWRevealViewController.m
+++ Friend Quiz/SWRevealViewController.m
@@ -275,6 +275,7 @@ static CGFloat scaledValue( CGFloat v1, CGFloat min2, CGFloat max2, CGFloat min1
 
 - (void)_layoutRearViewsForLocation:(CGFloat)xLocation
 {
+    //text
     CGRect bounds = self.bounds;
     
     CGFloat rearRevealWidth = _c.rearViewRevealWidth;
